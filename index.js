a8f855225f953ca697953668bb9779c9e67ad528
Joshua <yoonj200@gmail.com>
2022-02-17 21:09:05 -0600

    commit tree 98ca908ba0fe73a0ae3d54b55d76c529ae09b6c1

--- index.js
+++ index.js
@@ -1,4 +1,66 @@
 /* Your Code Here */
+const createEmployeeRecord = (recArray) => {
+    return {
+        firstName: recArray[0],
+        familyName: recArray[1],
+        title: recArray[2], 
+        payPerHour: recArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = (recordsArray) => {
+    return recordsArray.map(rec => createEmployeeRecord(rec))
+}
+
+const createTimeInEvent = function(dateStamp) {
+    const [date, hour] = dateStamp.split(" ")
+    // console.log('hour: ', hour);
+    // console.log('date: ', date);
+    const inEvent = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeInEvents.push(inEvent)
+    // console.log('this: ', this)
+    return this
+}
+
+const createTimeOutEvent = function (dateStamp) {
+    const [date, hour] = dateStamp.split(" ")
+
+    const outEvent = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeOutEvents.push(outEvent)
+    
+    return this
+}
+
+const hoursWorkedOnDate = function(targetDate) {
+    const inEvent = this.timeInEvents.find(inEvent => inEvent.date === targetDate)
+    const outEvent =  this.timeOutEvents.find(oEvent => oEvent.date === targetDate)
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+const wagesEarnedOnDate = function(targetDate) {
+    //return hoursWorkedOnDate.apply(this, [targetDate]) * this.payPerHour
+    return hoursWorkedOnDate.call(this, targetDate) * this.payPerHour
+}
+
+const findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(rec => rec.firstName === firstName)
+}
+
+const calculatePayroll = function(recsArray) {
+    return recsArray.reduce((total, rec) => {
+        return total + allWagesFor.call(rec)
+    }, 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -9,10 +71,12 @@
  for you to use if you need it!
  */
 
+ 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
+    //console.log('this: ', this);    
 
     const payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
@@ -20,4 +84,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
